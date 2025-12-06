88be922f7832d4fd08a893a769e5e8b7e80f3483
Faisal <zkryfysl@gmail.com>
2025-12-06 11:46:27 +0200

    make the delete comments and update them

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,57 +1,31 @@
-// import express from "express";
-// import {
-//     addProduct,
-//     listProducts,
-//     removeProduct,
-//     singleProduct,
-// } from "../controllers/productController.js";
-// import upload from "../middleware/multer.js";
-// import adminAuth from "../middleware/adminAuth.js";
-
-// const productRouter = express.Router();
-
-// productRouter.post(
-//     "/add",adminAuth,
-//     upload.fields([
-//         { name: "image1", maxCount: 1 },
-//         { name: "image2", maxCount: 1 },
-//         { name: "image3", maxCount: 1 },
-//         { name: "image4", maxCount: 1 },
-//     ]),
-//     addProduct
-// );
-// productRouter.post("/remove", adminAuth, removeProduct);
-// productRouter.post("/single", singleProduct);
-// productRouter.get("/list", listProducts);
-
-// export default productRouter;
-
-
 import express from "express";
 import {
-    addProduct,
-    listProducts,
-    removeProduct,
-    singleProduct,
-    addReviewToProduct, // استدعاء الدالة الجديدة
+  addProduct,
+  listProducts,
+  removeProduct,
+  singleProduct,
+  addReviewToProduct, // استدعاء الدالة الجديدة
+  listAllComments,
+  deleteComment,
+  updateComment,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
 import adminAuth from "../middleware/adminAuth.js";
-import auth from "../middleware/auth.js"; 
+import auth from "../middleware/auth.js";
 
 const productRouter = express.Router();
 
 // إضافة منتج (للمسؤول فقط)
 productRouter.post(
-    "/add",
-    adminAuth,
-    upload.fields([
-        { name: "image1", maxCount: 1 },
-        { name: "image2", maxCount: 1 },
-        { name: "image3", maxCount: 1 },
-        { name: "image4", maxCount: 1 },
-    ]),
-    addProduct
+  "/add",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
 );
 
 // حذف منتج (للمسؤول فقط)
@@ -65,5 +39,14 @@ productRouter.get("/list", listProducts);
 
 // إضافة تقييم لمنتج (يجب أن يكون المستخدم مسجّل دخول)
 productRouter.post("/review", auth, addReviewToProduct);
+
+// Admin: manage comments
+productRouter.get("/comments", adminAuth, listAllComments);
+productRouter.delete(
+  "/comments/:productId/:reviewId",
+  adminAuth,
+  deleteComment
+);
+productRouter.put("/comments/:productId/:reviewId", adminAuth, updateComment);
 
 export default productRouter;
